c2638d0ed0b5ffab7ebfd45441493c24e2388f5b
Vasileva Evgeniya <vasilevazhenechkac@mail.ru>
2025-02-14 15:39:47 +0500

    Изменена логика работы Main на вывод текста в консоли и подсчет длины текста

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите текст и нажмите <Enter>:");
         String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " +text.length());
+        System.out.println("Длина текста: " + text.length());
     }
-
 }
