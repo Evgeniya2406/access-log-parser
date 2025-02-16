26932fab27fbc5bf95bc2d6ddc3be00432266af9
Vasileva Evgeniya <vasilevazhenechkac@mail.ru>
2025-02-16 21:17:54 +0500

    Доработана логика вычисления и вывода в консоль суммы, разности, произведения и частного этих чисел.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.print("Сумма: ");
+        System.out.println(number + number2);
+        System.out.print("Разность: ");
+        System.out.println(number - number2);
+        System.out.print("Произведение: ");
+        System.out.println(number * number2);
+        System.out.print("Частное: ");
+        System.out.println((double) number / number2);
     }
 }
