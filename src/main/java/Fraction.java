f2d7ec3d3e00fc599574e8f6ffdd5e063a72a811
Vasileva Evgeniya <vasilevazhenechkac@mail.ru>
2025-03-18 00:38:13 +0500

    Практика ООП. Инкапсуляция #1. Дробь

---
+++ src/main/java/Fraction.java
@@ -0,0 +1,89 @@
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(){
+        this.numerator=0;
+        this.denominator=1;
+    }
+
+    public Fraction(int x){
+        this.numerator=x;
+        this.denominator=1;
+    }
+
+    public Fraction(int numerator, int denominator) {
+        if(denominator<=0) throw new IllegalArgumentException("Задано нулевое или отрицательное значение");
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction(Fraction fraction){
+        this.numerator= fraction.getNumerator();
+        this.denominator= fraction.getDenominator();
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+
+    @Override
+    public String toString() {
+        return "%d/%d".formatted(numerator,denominator);
+    }
+
+    public Fraction add(int x) {
+        Fraction fraction1 = new Fraction(x);
+        Fraction fraction2= new Fraction(this.numerator,this.denominator);
+        return fraction2.add(fraction1);
+    }
+    public Fraction substract(int x) {
+        Fraction fraction1 = new Fraction(x);
+        Fraction fraction2= new Fraction(this.numerator,this.denominator);
+        return fraction2.substract(fraction1);
+    }
+
+
+    public Fraction add(Fraction fraction2) {
+        int numerator1=0;
+        int denominator1=1;
+        if (denominator == fraction2.getDenominator()) {
+            numerator1 = numerator + fraction2.getNumerator();
+        }
+        else {
+            int result;
+            result=numerator * fraction2.getDenominator() + fraction2.getNumerator() * denominator;
+            numerator1=result;
+            denominator1 = denominator * fraction2.getDenominator();
+        }
+        return new Fraction(numerator1,denominator1);
+
+    }
+    public Fraction substract(Fraction fraction2) {
+        int numerator1=0;
+        int denominator1=1;
+        if (denominator == fraction2.getDenominator()) {
+            numerator = numerator - fraction2.getNumerator();
+        }
+        else {
+            int result;
+            result=numerator * fraction2.getDenominator() - fraction2.getNumerator() * denominator;
+            numerator1=result;
+            denominator1 = denominator * fraction2.getDenominator();
+        }
+        return new Fraction(numerator1,denominator1);
+    }
+}
--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,45 +1,18 @@
 public class Start {
     public static void main(String[] args) {
-        int[][] intPoints = {{1,5},{2,8},{5,3},{8,9}};
-       // int[][] intPoints = {{0,5},{5,5},{10,5},{15,5}};
-        Point[] points = new Point[intPoints.length];
-        for (int i = 0; i< intPoints.length; i++) {
-            points[i]=new Point(intPoints[i][0],intPoints[i][1]);
-        }
+        System.out.println("Задание 1: Создание экземпляров дробей: ");
+        Fraction f1 = new Fraction(1,3);
+        Fraction f2 = new Fraction(2,5);
+        Fraction f3 = new Fraction(7,8);
 
-        PolyLine polyLine = new PolyLine();
-        polyLine.setCoordinates(points);
-        System.out.println("Пункт задания 1:");
-        System.out.println(polyLine);
+        System.out.println(f1);
+        System.out.println(f2);
 
-        System.out.println("Пункт задания 2:");
-        System.out.println("Длина Ломаной равна: " + polyLine.getLength());
-
-        Line[] lines = new Line[]{new Line()};
-        lines =polyLine.getLines();
-        System.out.println("Пункт задания 3:");
-        for (int i = 0;i< lines.length;i++) {
-            System.out.println(lines[i]);
-        }
-
-        System.out.println("Пункт задания 4:");
-
-        Double length=0.0;
-        for (int i = 0; i<= lines.length-1; i++) {
-            length += lines[i].getLength();
-        }
-        System.out.println("Длина массива Линий: " + length);
-
-        System.out.println("Пункт задания 5:");
-        if (Math.round(polyLine.getLength()*1000)==Math.round(length*1000)){
-            System.out.println("Длина линий совпадает: " + Math.round(polyLine.getLength()*1000)/1000.0 +" = "+Math.round(length*1000)/1000.0);
-        } else {
-            System.out.println("Длина линий НЕ совпадает!");
-        }
-
-        System.out.println("Пункт задания 6:");
-        points[1].setX(12);
-        points[1].setY(8);
-        System.out.println(polyLine);
+        System.out.println("Задание 2: Посчитать f1.sum(f2).sum(f3).minus(5), где f1 это одна треть, f2 две пятых, а f3 это семь восьмых: ");
+        Fraction result1 = new Fraction( f1.add(f2).add(f3).substract(5));
+        System.out.println(result1);
+        System.out.println("Проверка: ");
+        Fraction result2 = new Fraction(result1.substract(f2).substract(f3).add(5));
+        System.out.println(result2);
     }
 }
