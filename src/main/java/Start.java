6a55035d6daaf56003eb69f8c4d85e9ffb293835
Vasileva Evgeniya <vasilevazhenechkac@mail.ru>
2025-03-30 06:29:59 +0500

    Задание #2. Сравнение объектов

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,11 +1,11 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class AccessLogException extends Exception{
     public AccessLogException(String msg){
         super(msg);
     }
-
 }
 
 public class Start {
@@ -16,47 +16,59 @@ public class Start {
             ex.printStackTrace();
         }
     }
-        public static void courseProject() throws IOException, AccessLogException {
-            boolean файлСуществует = false;
-            boolean папкаСуществует = false;
-            Scanner sc = new Scanner(System.in);
-            String path = "";
-            File file = new File(path);
-            int количествоСуществующихФайлов = 0;
 
-            while (!файлСуществует) {
-                System.out.println("Введите полный путь к файлу:  ");
-                path = sc.nextLine();
-                file = new File(path);
-                файлСуществует = file.isFile();
-                папкаСуществует = file.isDirectory();
-                if (!файлСуществует || папкаСуществует) {
-                    System.out.println("Файл не существует или путь является путём к папке, а не к файлу");
-                    continue;
-                } else if (файлСуществует) {
-                    количествоСуществующихФайлов += 1;
-                    System.out.println("Путь указан верно");
-                }
+    public static void courseProject() throws IOException, AccessLogException {
+        boolean файлСуществует = false;
+        boolean папкаСуществует = false;
+        Scanner sc = new Scanner(System.in);
+        String path = "";
+        File file = new File(path);
+        int количествоСуществующихФайлов = 0;
 
-
-                FileReader fileReader = new FileReader(path);
-                BufferedReader reader =
-                        new BufferedReader(fileReader);
-                String line;
-                line = reader.readLine();
-                Integer max = line.length();
-                Integer min = line.length();
-                Integer count = 1;
-                while ((line = reader.readLine()) != null) {
-                    count++;
-                    int length = line.length();
-                    if (length>max) max=length;
-                    if (length<min) min=length;
-                    if (max>=1024) throw new AccessLogException("строка равна или длиннее 1024 символов");
-                }
-                System.out.println("Общее количество строк: " + count);
-                System.out.println("Длина самой длинной строки: " + max);
-                System.out.println("Длина самой короткой строки: " +min);
+        while (!файлСуществует) {
+            System.out.println("Введите полный путь к файлу:  ");
+            path = sc.nextLine();
+            file = new File(path);
+            файлСуществует = file.isFile();
+            папкаСуществует = file.isDirectory();
+            if (!файлСуществует || папкаСуществует) {
+                System.out.println("Файл не существует или путь является путём к папке, а не к файлу");
+                continue;
+            } else if (файлСуществует) {
+                количествоСуществующихФайлов += 1;
+                System.out.println("Путь указан верно");
             }
+
+
+            FileReader fileReader = new FileReader(path);
+            BufferedReader reader =
+                    new BufferedReader(fileReader);
+            String line;
+            line = reader.readLine();
+            Integer count = 1;
+            Integer yandexBotCounter =0;
+            Integer googleBotCounter =0;
+
+            while ((line = reader.readLine()) != null) {
+                if (parseString(line).getLast().contains("YandexBot")) yandexBotCounter++;
+                if (parseString(line).getLast().contains("Googlebot")) googleBotCounter++;
+                count++;
+                int length = line.length();
+                if (length >= 1024) throw new AccessLogException("строка равна или длиннее 1024 символов");
+            }
+            System.out.println("Доля запросов YandexBot: " + Math.round(yandexBotCounter*1.0/count*1000.0)/10.0);
+            System.out.println("Доля запросов Googlebot: " + Math.round(googleBotCounter*1.0/count*1000.0)/10.0);
+            System.out.println("Общее количество запросов: " + count);
         }
     }
+
+    private static ArrayList<String> parseString(String str) {
+        ArrayList<String> arr = new ArrayList<>();
+        String str1=str.substring(0,str.length()-2);
+        int start = str1.lastIndexOf("\"");
+        arr.add(str1.substring(start+1));
+        return arr;
+    }
+
+}
+
